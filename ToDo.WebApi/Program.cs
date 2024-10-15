29cb8cb7e8cd674f34f282b8b0440e84f27078a3
Piotr Stus <pm2024love@gmail.com>
2024-10-15 22:05:51 +0200

    Serilog added.

--- ToDo.WebApi/Program.cs
+++ ToDo.WebApi/Program.cs
@@ -1,10 +1,29 @@
+using Serilog;
+
 namespace ToDo.WebApi
 {
     public class Program
     {
+        public static string APP_NAME = "ToDo.WebApi";
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog();
+
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext());
 
             // Add services to the container.
 
