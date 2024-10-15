0e6e4064442497653ad989c114fbb60c9a13f0e7
Piotr Stus <pm2024love@gmail.com>
2024-10-15 22:43:54 +0200

    Entity - Items designed.

---
+++ Todo.Domain/Entities/Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Common;
+
+namespace ToDo.Domain.Entities
+{
+    public class Items : DomainEntity
+    {
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public required DateTimeOffset DueDate { get; set; }
+
+        public required bool IsCompleted { get; set; }
+
+        public required DateTimeOffset CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
